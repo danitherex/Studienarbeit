fe5ff3e87d8a75054ef6fe09a05e31eaa3324f97
danitherex <daniel@maurer-koblenz.de>
2023-12-15 17:52:48 +0100

    added circle

--- scr/app/src/main/java/com/example/studienarbeit/Location.kt
+++ scr/app/src/main/java/com/example/studienarbeit/Location.kt
@@ -1,16 +1,23 @@
 package com.example.studienarbeit
 
-import Permissions
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.IntentSender
+import android.content.pm.PackageManager
 import android.os.Looper
 import android.util.Log
+import androidx.activity.result.IntentSenderRequest
 import androidx.compose.runtime.*
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.tasks.CancellationToken
 import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.gms.tasks.OnTokenCanceledListener
@@ -24,14 +31,12 @@ lateinit var locationProvider: FusedLocationProviderClient
 const val LOCATION_TAG = "LOCATION"
 
 
-@SuppressLint("MissingPermission")
 @Composable
-fun getUserLocation(context: Context): LatandLong {
+fun getUserLocation(context: Context): LatLng {
 
     // The Fused Location Provider provides access to location APIs.
     locationProvider = LocationServices.getFusedLocationProviderClient(context)
-
-    var currentUserLocation by remember { mutableStateOf(LatandLong()) }
+    var currentUserLocation by remember { mutableStateOf(LatLng(0.0, 0.0)) }
 
 
     DisposableEffect(key1 = locationProvider) {
@@ -39,10 +44,28 @@ fun getUserLocation(context: Context): LatandLong {
             //1
             override fun onLocationResult(result: LocationResult) {
 
+                if (ActivityCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
                 locationProvider.getCurrentLocation(
                     LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY,
                     object : CancellationToken() {
-                        override fun onCanceledRequested(p0: OnTokenCanceledListener) = CancellationTokenSource().token
+                        override fun onCanceledRequested(p0: OnTokenCanceledListener) =
+                            CancellationTokenSource().token
 
                         override fun isCancellationRequested() = false
 
@@ -52,7 +75,7 @@ fun getUserLocation(context: Context): LatandLong {
                         val long = location.longitude
                         Log.d("Location_success", "Latitude: $lat, Longitude: $long")
                         // Update data class with location data
-                        currentUserLocation = LatandLong(latitude = lat, longitude = long)
+                        currentUserLocation = LatLng(lat, long)
                     }
                 }
                     .addOnFailureListener {
@@ -63,7 +86,7 @@ fun getUserLocation(context: Context): LatandLong {
             }
         }
 
-        locationUpdate()
+        locationUpdate(context)
         onDispose {
             stopLocationUpdate()
         }
@@ -72,17 +95,36 @@ fun getUserLocation(context: Context): LatandLong {
     return currentUserLocation
 }
 
-//data class to store the user Latitude and longitude
-data class LatandLong(
-    val latitude: Double = 0.0,
-    val longitude: Double = 0.0
-)
 
 @SuppressLint("MissingPermission")
-fun locationUpdate() {
+fun locationUpdate(context: Context) {
     val locationRequest = LocationRequest.Builder(10000)
         .setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY)
         .build()
+
+    val client = LocationServices.getSettingsClient(context)
+    val builder = LocationSettingsRequest
+        .Builder()
+        .addLocationRequest(locationRequest)
+
+    val gpsSettingTask = client.checkLocationSettings(builder.build())
+
+    gpsSettingTask.addOnSuccessListener {
+        Log.d(LOCATION_TAG, "Settings Ok")
+    }
+    gpsSettingTask.addOnFailureListener { exception ->
+        if (exception is ResolvableApiException) {
+            try {
+                val intentSenderRequest = IntentSenderRequest
+                    .Builder(exception.resolution)
+                    .build()
+                Log.d(LOCATION_TAG, "Settings Bad")
+            } catch (sendEx: IntentSender.SendIntentException) {
+                // ignore here
+            }
+        }
+    }
+
 
     locationCallback.let {
         locationProvider.requestLocationUpdates(
--- scr/app/src/main/java/com/example/studienarbeit/Map.kt
+++ scr/app/src/main/java/com/example/studienarbeit/Map.kt
@@ -10,11 +10,17 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.Circle
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
@@ -25,19 +31,18 @@ import com.google.maps.android.compose.rememberCameraPositionState
 fun Map() {
     val context = LocalContext.current
 
-    var location: LatandLong? = null
-    location = getUserLocation(context)
+    var currentLocation by remember { mutableStateOf(LatLng(0.0, 0.0)) }
+    currentLocation = getUserLocation(context)
 
-    val currentLocation = LatLng(location.latitude, location.longitude)
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(currentLocation, 10f)
     }
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
                 shape = AbsoluteRoundedCornerShape(50.dp),
                 onClick = {
-
 
                 }) {
                 Icon(Icons.Default.MyLocation, "My Location")
@@ -46,7 +51,7 @@ fun Map() {
         modifier = Modifier
             .fillMaxSize()
     )
-    {innerPadding ->
+    { innerPadding ->
         Permissions()
         GoogleMap(
             modifier = Modifier
@@ -59,9 +64,16 @@ fun Map() {
         ) {
             Marker(
                 state = MarkerState(position = currentLocation),
-                title = "Singapore",
-                snippet = "Marker in Singapore"
+                title = "Current Location",
+                snippet = "Marker in current location"
             )
+            Circle(
+                center = currentLocation,
+                strokeColor = Color.Black,
+                radius = 450.0,
+                strokeWidth = 7.0F,
+
+                )
         }
     }
 }
