874df4b62a61f3f0e91aab12b93a13566e22d633
Daniel Maurer <daniel@maurer-koblenz.de>
2024-01-29 11:54:25 +0100

    added confirm delete dialog

---
+++ scr/app/src/main/java/com/example/studienarbeit/presentation/map/components/ConfirmDeleteDialog.kt
@@ -0,0 +1,36 @@
+package com.example.studienarbeit.presentation.map.components
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ConfirmDeleteDialog(
+    onConfirm: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDismissRequest: () -> Unit,
+    title:String
+) {
+    AlertDialog(
+        modifier = modifier,
+        onDismissRequest = onDismissRequest,
+        confirmButton = {
+            TextButton(onClick = onConfirm) {
+                Text(text = "Delete")
+            }
+        },
+        title = {
+            Text(text = "Delete Marker")
+        },
+        text = {
+            Text(text = "Do you want to delete your marker $title?")
+        },
+        dismissButton = {
+            TextButton(onClick = onDismissRequest) {
+                Text(text = "Cancel")
+            }
+        }
+    )
+}
